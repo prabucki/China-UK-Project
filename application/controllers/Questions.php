038c188b0344c0a1676064099a0ac560dc70b81f
Tomek Prabucki <tom.prabucki@gmail.com>
2017-04-14 03:44:07 +0100

    Code cleaning

--- application/controllers/Questions.php
+++ application/controllers/Questions.php
@@ -21,5 +21,7 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('welcome_message');
+		$question = $this->db->get_where('questions_uk', array('id' => 1));
+		var_dump($query);
 	}
 }
