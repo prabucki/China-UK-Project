726a04870786370d900f70363a7376a46d99b00c
JuneFXJ <Junefxj@163.com>
2017-04-19 13:16:36 +0800

    trueOrFalse bg change

--- application/views/trueOrFalse.php
+++ application/views/trueOrFalse.php
@@ -41,46 +41,40 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     </script>
 </head>
 <style>
-    .header {
-        width: 100%;
-        height: 120px;
-        background-color: lightblue;
-        margin: 0;
-        padding-left: 10px;
+    body {
+        background-image: url("../../image/map.jpg");
+        background-size: 100%;
+        z-index: -1;
     }
-    h1 {
-        font-size: 60px;
-        margin: 0px;
-        padding: 0px;
-        line-height: 120px;
-        color: white;
-    }
-
     .questionDisplay {
         text-align: center;
-        margin-top: 100px;
+        margin: 10% 20%;
+        opacity: 0.9;
+        /*filter:alpha(opacity=50);*/
+        background: gainsboro;
+        width: 60%;
+        height: 80%;
+        border-radius: 15px;
+        /*padding-top: 40px;*/
+        padding: 30px 0;
     }
     .blankRow{
-        height:80px;
+        height: 90px;
         width:100%;
         /*background-color: brown;*/
         margin: 0px;
     }
     .button{
-        margin: 40px 60px;
+        margin: 15px 70px;
         /*background-color: #e38d13;*/
-    }
-    .footer {
-        width: 100%;
-        height: 120px;
-        background-color: lightblue;
-        margin: 0;
+        font-size: 24px;
+        line-height: 24px;
     }
 </style>
-<body style="margin: 0">
-<div class="header">
-    <h1>Know Culture</h1>
-</div>
+<body>
+<!--<div class="header">-->
+<!--    <h1>Know Culture</h1>-->
+<!--</div>-->
 <div class="questionDisplay">
     <form class="form-horizontal" role="form">
         <input type="hidden" id="question_id" name="question_id" value="<?php echo $id;?>">
@@ -90,28 +84,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <input type="hidden" id="status" name="status" value="<?php echo $status;?>">
         <!--The no. and questions here-->
         <div class="form-group">
-            <label class="col-lg-2 control-label input-lg" id="no"><h2>1.</h2></label>
-            <label class="col-lg-7 control-label input-lg" name="question"><h2><?php echo $question;?></h2></label>
+            <label class="col-lg-3 control-label input-lg" id="no"><h2>1.</h2></label>
+            <label class="col-lg-3 control-label input-lg" name="question"><h2><?php echo $question; ?></h2></label>
         </div>
         <div class="blankRow"></div>
         <div class="form-group">
-            <div class="col-lg-offset-3 col-lg-10 input-lg">
+            <div class="col-lg-offset-2 input-lg">
                 <button id="false" class="btn btn-info btn-lg input-lg col-lg-2 button">False</button>
                 <button id="true" class="btn btn-info btn-lg input-lg col-lg-2 button">True</button>
             </div>
         </div>
     </form>
 </div>
-<div class="footer navbar-fixed-bottom"></div>
-
-<script>
-    var answer = 0;
-    function falseClick(){
-
-    }
-    function trueClick(){
-        answer = 1;
-    }
-</script>
+<!--<div class="footer navbar-fixed-bottom"></div>-->
 </body>
 </html>
